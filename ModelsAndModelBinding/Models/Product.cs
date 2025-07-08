be5e0e36761727fcdbbb97848341a6656848b1b9
Logan Nelson <Lnelson0915@gmail.com>
2025-07-08 09:59:43 -0700

    Add Product class to ModelsAndModelBinding.Models
    Introduced a new `Product` class representing individual salable
    products. The class includes properties for `ProductId`,`Name`,`Price`, and `StartSalesDate`.

---
+++ ModelsAndModelBinding/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace ModelsAndModelBinding.Models;
+
+/// <summary>
+/// Represents an individual salable product
+/// </summary>
+public class Product
+{
+    /// <summary>
+    /// A unique identifier for the product.
+    /// The primary key of the product
+    /// </summary>
+    public int ProductId { get; set; }
+
+    /// <summary>
+    /// The consumer facing name of the product. May
+    /// contain alphanumeric characters.
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// The pre-tax sales price the customer must pay.
+    /// </summary>
+    public double Price { get; set; }
+
+    /// <summary>
+    /// The date and time the product became
+    /// available for sale.
+    /// </summary>
+    public DateTime StartSalesDate { get; set; }
+}
